370d13b13069b3e5db90bbab3922a1ad79236326
Szymon <kontakt@szymonkadelski.pl>
2023-12-27 14:43:08 +0100

    Bank Days Entity -> GET, POST, DELETE -> add to callculation of the vacations days -> bug repair

--- src/EventSubscriber/DepartmentDeleteValidationSubscriber.php
+++ src/EventSubscriber/DepartmentDeleteValidationSubscriber.php
@@ -2,30 +2,33 @@
 
 namespace App\EventSubscriber;
 
+use ApiPlatform\Symfony\EventListener\EventPriorities;
 use App\Entity\Company\Department;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Exception\BadRequestException;
+use Symfony\Component\HttpKernel\KernelEvents;
 
 class DepartmentDeleteValidationSubscriber implements EventSubscriberInterface
 {
     public function onPreDelete($event): void
     {
-        $department = $event->getRequest()->attributes->get("data");
+        $department = $event->getControllerResult();
 
         if($department instanceof Department)
         {
-            if(!empty($department->getEmployees()))
+            if(!empty($department->getEmployees()->toArray()))
             {
                 throw new BadRequestException("Nie można usunąć działu, do teog działu są przypisani pracownicy.");
             }
         }
-
     }
 
     public static function getSubscribedEvents(): array
     {
         return [
-            'pre_delete' => 'onPreDelete',
+            KernelEvents::VIEW => [
+                'onPreDelete', EventPriorities::PRE_WRITE
+            ]
         ];
     }
 }
